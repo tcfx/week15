f8e01caab62cd3f3cb6e0f57f3aabc84b0a98030
tcfx <leeanne1212@live.ca>
2022-06-17 23:00:51 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+
+function procceed(){
+
+    alert("Thank You! We will send you more information");
+
+    $("procceed").css("background-color", "lavender");
+}
+
+$(window).on("click", procceed);
+
+
+function search(){
+
+    alert("Due to Redesigns of the site, We can not complete that option. We will have this issues fixed soon!"
+    );
+
+    $("search").css("background-color", "lavender");
+}
+
+$(window).on("click", search);
